983fc2fa69a7b2d9b514415b386207c250f91904
SatrioWahid <aidmhrm73@gmail.com>
2026-03-04 10:19:11 +0700

    Add files via upload

---
+++ latihan5.1.php
@@ -0,0 +1,17 @@
+<?php
+$nilai = -31;
+
+if ($nilai > 100) {
+echo "Maaf Nilai Tidak Terdeteksi";
+}
+elseif ($nilai >= 60) {
+echo "Lulus";
+}
+elseif ($nilai < 0 ) {
+echo "Maaf Nilai Tidak Terdeteksi";
+}
+else {
+echo "Tidak Lulus";
+}
+?>
+
