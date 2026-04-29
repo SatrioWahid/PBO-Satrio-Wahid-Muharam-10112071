1817f7353ec5335ff949e1a3d3180254d6a0bc41
SatrioWahid <aidmhrm73@gmail.com>
2026-04-29 09:58:49 +0700

    Add files via upload

---
+++ latihan9.3.php
@@ -0,0 +1,78 @@
+<?php
+
+// buat class komputer
+class komputer {
+
+    // Diubah menjadi protected agar bisa diakses oleh class laptop
+    protected $jenis_processor = "Intel Core i7-4790 3.6Ghz";
+    protected $jenis_RAM = "DDR 4";
+    public $jenis_VGA = "PCI Express";
+
+    public function tampilkan_processor() {
+        return $this->jenis_processor;
+    }
+
+    public function tampilkan_jenisprocessor() {
+        return $this->jenis_processor;
+    }
+
+    // Diubah menjadi protected agar bisa dipanggil di class laptop
+    protected function tampilkan_ram() {
+        return $this->jenis_RAM;
+    }
+
+    // Tetap protected karena dipanggil via method public di class anak
+    protected function tampilkan_vga() {
+        return $this->jenis_VGA;
+    }
+
+    public function tampilkan_vga2() {
+        return $this->jenis_VGA;
+    }
+
+}
+
+// buat class laptop
+class laptop extends komputer {
+
+    public function display_processor() {
+        return $this->jenis_processor;
+    }
+
+    public function display_processor2() {
+        return $this->tampilkan_processor();
+    }
+
+    public function display_ram() {
+        return $this->jenis_RAM;
+    }
+
+    public function display_ram2() {
+        return $this->tampilkan_ram(); // Sekarang bisa karena method di ortu sudah protected
+    }
+
+    public function display_vga() {
+        return $this->tampilkan_vga();
+    }
+
+    // Diubah menjadi public agar bisa dipanggil dari objek $laptop di luar class
+    public function display_processorkomputer() {
+        return $this->jenis_processor;
+    }
+
+}
+
+// buat objek (instansiasi)
+$komputer = new komputer();
+$laptop = new laptop();
+
+// jalankan method
+echo "Line 61 : ".$komputer->tampilkan_processor()."<br />";
+echo "Line 62 : ".$laptop->display_processor()."<br />";
+echo "Line 63 : ".$laptop->display_processor2()."<br />";
+echo "Line 64 : ".$laptop->tampilkan_jenisprocessor()."<br />";
+echo "Line 65 : ".$laptop->display_ram()."<br />";
+echo "Line 66 : ".$laptop->display_vga()."<br />";
+echo "Line 67 : ".$laptop->display_processorkomputer()."<br />";
+
+?>
